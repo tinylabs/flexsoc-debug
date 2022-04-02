e96acb768122f15d096125f2f74b91f0d4927cbc
Tiny Labs Inc <elliot@tinylabs.io>
2022-04-02 15:44:57 -0600

    Fix libftdi deprecated call

--- host/flexsoc/FTDITransport.cpp
+++ host/flexsoc/FTDITransport.cpp
@@ -62,7 +62,7 @@ int FTDITransport::Open (char *id)
     log (LOG_FATAL, "Failed to set interface");
   
   // Purge buffers
-  rv = ftdi_usb_purge_buffers (ftdi);
+  rv = ftdi_tcioflush (ftdi);
   if (rv)
     log (LOG_FATAL, "Failed to purge FTDI buffers!");
 
@@ -107,7 +107,7 @@ void FTDITransport::Close (void)
 
 void FTDITransport::Flush (void)
 {
-  if (ftdi_usb_purge_buffers (ftdi))
+  if (ftdi_tcioflush (ftdi))
     log (LOG_FATAL, "Failed to purge buffers");
 }
 
